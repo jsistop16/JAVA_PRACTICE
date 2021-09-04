52a47d8510d37f0361b5675615d309d0efc26b39
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2021-09-04 21:47:02 +0900

    final

---
+++ 기본코드/abstractclass/FinalPractice2.java
@@ -0,0 +1,30 @@
+package org.javatutorials.abstractclass;
+
+final class E{
+	
+}
+
+//class F extends E{
+	//에러 발생 : final로 지정된 A클래스는 상속불가
+//}
+
+class C{
+	final void abs(){
+	}
+}
+
+//class D extends C{
+//	void abs() {
+//		//에러 발생: final로 지정된 abs메소드는 오버라이딩 수정 불가
+//	}
+//}
+
+
+public class FinalPractice2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
