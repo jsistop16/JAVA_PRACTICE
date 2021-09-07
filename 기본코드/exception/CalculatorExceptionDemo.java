3a005f03de1bcf0d6a545bf51940fd6f8bc4ec98
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2021-09-07 23:41:32 +0900

    사용자정의 예외처리

--- 기본코드/exception/CalculatorExceptionDemo.java
+++ 기본코드/exception/CalculatorExceptionDemo.java
@@ -1,5 +1,28 @@
 package org.javatutorials.exception;
 
+class DivideException extends RuntimeException{//사용자정의 예외 
+	
+	int left, right;
+	
+	DivideException(){
+		super();
+	}
+	
+	DivideException(String message){
+		super(message);
+	}
+	
+	DivideException(String message, int left, int right){
+		//사용자정의 예외에 특별한 기능을 추가할수도있음
+		//어떤 변수에 의해서 DivideException이 발생했는지
+		//출력해주는 생성자
+		super(message);
+		this.left = left;
+		this.right = right;
+	}
+	
+}
+
 class Calculator{
 	int left, right;
 	
@@ -8,20 +31,11 @@ class Calculator{
 		this.right = right;
 	}
 	
-	public void divide() {
+	public void divide() throws DivideException {
 		if (right == 0) {
-			throw new ArithmeticException("0으로 나눌수없습니다.");
+			throw new DivideException("0으로 나눌수없습니다.", left, right);//ㄱ
 		}
-		try {
-			System.out.println("실행결과는 ");
-			System.out.println(this.left / this.right);
-			System.out.println("입니다.");
-		}
-		catch(Exception e) {
-			System.out.println("오류내용: "+ e.getMessage());
-			System.out.println("오류내용: "+ e.toString());
-			e.printStackTrace();
-		} 
+		System.out.println(this.left / this.right);
 	}
 }
 
@@ -33,8 +47,10 @@ public class CalculatorExceptionDemo {
 		try {
 			c1.divide();
 		}
-		catch(ArithmeticException e){
+		catch(DivideException e){//ㄱ에서 선언된 예외가 전달됨
 			System.out.println(e.getMessage());
+			System.out.println(e.left);
+			System.out.println(e.right);
 		}
 	}
 
